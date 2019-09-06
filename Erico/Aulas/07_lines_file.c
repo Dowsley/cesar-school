30f2a40e3de584da0b712ec76257e5045ea2e9b3
João Dowsley <joao.dowsley850@gmail.com>
2019-09-05 22:00:46 -0300

    05/09

---
+++ Erico/Aulas/07_lines_file.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define FILENAME "08_lines_file.c"
+
+int main(){
+		FILE *fp;
+		char ch;
+		int linesCount=0;
+
+		//open file in read more
+		fp=fopen(FILENAME,"r");
+		if(fp==NULL){
+				printf("File \"%s\" does not exist!!!\n",FILENAME);
+				return -1;
+		}
+
+		//read character by character and check for new line
+		while((ch=fgetc(fp))!=EOF) {
+				if(ch=='\n')
+						linesCount++;
+		}
+
+		//close the file
+		fclose(fp);
+
+		//print number of lines
+		printf("Total number of lines are: %d\n",linesCount);
+
+		return 0;
+}
