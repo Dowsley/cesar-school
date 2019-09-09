d626a676aedcab54ff2dcc71751848a2ccfbc508
João Dowsley <joao.dowsley850@gmail.com>
2019-09-08 23:26:09 -0300

    08/09

--- Erico/Desafios/06_linkedList_operations.c
+++ Erico/Desafios/06_linkedList_operations.c
@@ -9,61 +9,29 @@ typedef struct node{
 }node;
 
 void add(node**, int);
-void exibir(node**);
+void remove(node**, int);
+void display(node**);
 
 
 /***** PROGRAMA PRINCIPAL *****/
 int main()
 {
-	node* head = NULL;
 
-	add(&head, 4);
-	add(&head, 2);
-	add(&head, 3);
-	add(&head, 10);
-
-	exibir(&head);
-
-	return 0;
 }
 
 
 void add(node** head, int novo_valor)
 {
-	node* novo_node = malloc(sizeof(node*));
+	
+}
 
-	/* Cria um novo node, depois decide onde irá inserir */
-	(*novo_node).valor = novo_valor;
-	(*novo_node).prox = NULL;
-
-	if (*head == NULL)   // Encontrou a cauda
-		*head = novo_node;
-	else   // Vai procurar a cauda
-	{
-		node* atual = *head;
-
-		while ((*atual).prox != NULL)   // Procurando cauda
-			atual   = (*atual).prox;   // Passa de node pra node
-
-		(*atual).prox = novo_node;   // Coloca o novo node na cauda null.
-	}
+void remove(node** head, int novo_valor)
+{
+	
 }
 
 
-void exibir(node** head)
+void display(node** head)
 {
-		printf("LISTA");
-        if (head == NULL)
-            printf(" -> \\");
-        else
-        {
-            node* atual = *head;
-            while (atual != NULL)
-            {
-                printf(" -> %d", atual->valor);
-                atual = atual->prox;
-            }
-            if (atual == NULL)
-                printf(" -> \\");
-        }
+
 }
---
+++ Hazin/Desafios/caixas.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	int n;   // Número de caixas total (Suas numerações vão de 1 a n)
+	int p;	 // Número de pilhas (Tem que ser menor ou igual à quantidade de caixas)
+	int q;   // Número de caixas na dada linha
+	int id;   // Identificador de caixa
+
+
+	for (int i = 0; p > 0; p--)
+	{
+		scanf("%d", &q);
+		for (; q > 0; q--)
+		{
+			scanf("%d", &id);
+			/* insere */
+		}
+	}
+
+	return 0;
+}
