37e804804a6f76192546f95b56707f6595f9c431
Dowsley <joao.dowsley850@gmail.com>
2019-09-20 10:31:07 -0300

    Ok

--- Erico/Prova1/q6.c
+++ Erico/Prova1/q6.c
@@ -2,30 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef void *callback (int*,int)
+int par(int n)
 {
-
-}callback;
-
-void par(int* array, int len)
-{
-
+	if ( (n%2) == 0 && n != 0)
+		printf("%d ", n);
 }
 
-void impar(int* array, int len)
-{
 
+int impar(int n)
+{
+	if ( (n%2) != 0 && n != 0)
+		printf("%d ", n);
+}
+
+void callback(int* array, int (*p)(int))
+{
+	int len = sizeof(array) / sizeof(int);
+	for (int i = 0; i < len; i++)
+		(*p)(array[i]);
 }
 
 int main(int argc, char *argv[])
 {
+	char choice = argv[0][0];
 	int len = argc - 1;
 	int* array = malloc(len * sizeof(int));
 
 	for (int i = 1; i < argc; i++)
 		array[i-1] = atoi(argv[i]);
 
-		
+	if (choice == 'p')
+		int (*p)(int) = &par;
+	else
+		int (*p)(int) = &impar;
+
+	callback(array, p);
 
 	return 0;
 }
