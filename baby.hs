71ceb46446d79b1cda8f699eddba532f73a465ea
Dowsley <joao.dowsley850@gmail.com>
2019-09-23 20:29:17 -0300

    Primeira Aula de Haskell

---
+++ baby.hs
@@ -0,0 +1,18 @@
+-- Constantemente retorna "n" vezes o fatorial do número -1, e só para a recursão quando...
+-- ...o "n" é zero, onde o resultado é 1.
+fac :: Int -> Int
+fac 0 = 1
+fac n = n * fac(n-1)
+
+
+-- Retorna "x" vezes a recursão e decrementa y, só para quando o y chega em 0 e retorna 1, termo neutro também.
+power :: Int -> Int -> Int
+power _ 0 = 1
+power x y = x * power x (y-1)
+
+
+-- (A) Retorna 2 elevado a x, utilizando recursão e (x == 0) como ponto de parada.
+-- De certa forma diz a qtd de possibilidades de um número de X bits.
+bits :: Int -> Int
+bits 0 = 1
+bits x = 2 * bits (x-1)
