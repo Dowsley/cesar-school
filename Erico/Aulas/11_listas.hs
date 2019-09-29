3d7455d5fb74882d97c855e55fc2f493a83380c1
João Dowsley <joao.dowsley850@gmail.com>
2019-09-29 14:45:26 -0300

    Atualizado: Aula 10 de Érico

--- Erico/Aulas/11_listas.hs
+++ Erico/Aulas/11_listas.hs
@@ -1,14 +1,25 @@
--- Retorna o maior elemento de uma lista, usa recursividade.
-maxList :: [Int] -> Int
-maxList [] = 0
-maxList (a:as) | a > maxList as = a
+doubleList :: [Int] -> [Int]
+doubleList as | as == [] = []      
+              | otherwise = (head as)*2 : doubleList (tail as)
+
+doubleList' :: [Int] -> [Int]
+doubleList' [] = []
+doubleList' (a:as) = a*2 : doubleList' as
+
+maxList :: [Int] -> [Int]
+maxList [] = []
+maxList (a:[]) = [a]
+maxList (a:as) | a > head(maxList as) = [a]
                | otherwise = maxList as
 
+doubleList'' :: [Int] -> [Int]
+doubleList'' [] = []
+doubleList'' a = [b*2| b <- a]
 
--- Quicksort
 quicksort :: [Int] -> [Int]
 quicksort [] = []
-quicksort (x:xs) = quicksort (small xs x) ++ (x : quicksort(large xs x))
+quicksort (x:xs) = 
+quicksort (small xs x ) ++ (x : quicksort (large xs x))
 
 
 -- ATIVIDADE (DESAFIO 11): CRIE UMA FUNÇÃO SMALL E UMA LARGE PARA FAZER O QUICKSORT FUNCIONAR
