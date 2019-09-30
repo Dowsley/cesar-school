e96681309120d96fbb56970dd1e8ba3b0432921f
Dowsley <joao.dowsley850@gmail.com>
2019-09-30 12:03:09 -0300

    Novo: Aula 12 de Erico

--- Erico/Aulas/11_listas.hs
+++ Erico/Aulas/11_listas.hs
@@ -24,3 +24,12 @@ quicksort (small xs x ) ++ (x : quicksort (large xs x))
 
 -- ATIVIDADE (DESAFIO 11): CRIE UMA FUNÇÃO SMALL E UMA LARGE PARA FAZER O QUICKSORT FUNCIONAR
 -- DICA: USE COMPREENSÃO DE LISTA
+
+
+small :: [Int] -> Int -> [Int]   -- Retorna tudo que é menor que x na lista xs
+small xs x = [i | i <- xs, i < x]
+
+large :: [Int] -> Int -> [Int]   -- Retorna tudo que é maior que x na lista xs
+large xs x = [i | i <- xs, i > x]
+
+-- Feito.
---
+++ Erico/Aulas/12_tuplaTipos.hs
@@ -0,0 +1,41 @@
+-------------------- Aula 12 --------------------
+-- Soma elementos de uma tupla
+addPair :: (Int, Int) -> Int
+addPair (x,y) = x + y
+
+-- Soma elementos de diversas tuplas. Pode ser feito com recursão.
+addPairs :: [(Int, Int)] -> [Int]
+addPairs [] = []
+addPairs ls = [ (fst x) + (snd x) | x <- ls ] -- ou addPairs ls = [ a+b | (a,b) <- ls ]
+
+-- Retorna os caracteres q sao digitos
+digits :: String -> String
+digits lc = [ c | c <- lc, c `elem` ['0'..'9'] ]
+
+-- Retorna tamanho
+len :: [a] -> Int
+len [] = 0
+len (x:xs) = 1 + len xs
+
+
+--------------------- DESAFIO 12: FAZER UMA FUNÇÃO "ZIP" e "TAKE"
+zip' :: [a] -> [b] -> [(a,b)]
+zip' la lb = [  (la !! y, lb !! y) | y <- [0..((min (length la) (length lb)) - 1)]]
+-- NOTA: A função acima trava quando uma das entradas é infinita.
+-- Enquanto a função abaixo não
+
+-- zip' :: [t] -> [u] -> [(t,u)]
+-- zip' (a:as) (b:bs) = (a,b):zip' as bs
+-- zip' _ _ = []
+
+take' :: Int -> [a] -> [a]
+take' 0 _ = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n-1) xs
+-- NÃO QUEBRA COM LISTA INFINITA!
+
+-- 
+curry' :: ((a,b) -> c) -> a-> b -> c
+curry' f a b = f (a,b)
+
+-- DESAFIO 12.2: FAZER FUNAO UNCURRY (RECEBE UM OPERADOR E UMA TUPLA, DEPOIS FAZ OPERAÇÃO --> (*) (3,2) == 6)
