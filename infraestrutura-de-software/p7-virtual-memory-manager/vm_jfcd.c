4df44ec5c539c51f1b11db9f1e9335612e3ef547
Dowsley <joao.dowsley850@gmail.com>
2020-05-21 22:29:16 -0300

    Fica pra outra vez

--- infraestrutura-de-software/p7-virtual-memory-manager/vm_jfcd.c
+++ infraestrutura-de-software/p7-virtual-memory-manager/vm_jfcd.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_bits(size_t const size, void const * const ptr)
+{
+    unsigned char *b = (unsigned char*) ptr;
+    unsigned char byte;
+    int i, j;
+
+    for (i=size-1;i>=0;i--) {
+        for (j=7;j>=0;j--) {
+            byte = (b[i] >> j) & 1;
+            printf("%u", byte);
+        }
+    }
+    printf("\n\n");
+}
+
 int main(int argc, char const *argv[])
 {
-	/* Code! */
+	int mask = 0xFFFF; /* Bitmask to turn off the first 16 digits */
+	int p;
+
+	FILE *fp = fopen("BACKING_STORE.bin","rb");
+	
+	for (int i = 0; i < 5; i++) {
+		fread(&p, sizeof(int), 1, fp);
+		printf("normal %d: ", i);
+		print_bits(sizeof(p), &p);
+		p = p & mask;
+		printf("mask %d: ", i);
+		print_bits(sizeof(p), &p);
+	}
+	
+	fclose(fp);
 	return 0;
 }
